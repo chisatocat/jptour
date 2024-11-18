4e28468a9bb6b8a8041779b768d854379fb96315
Dickens Au <dickens721@gmail.com>
2024-11-18 23:59:48 +0800

    erb group project 20241118b

--- js/data/resource.js
+++ js/data/resource.js
@@ -1,5 +1,5 @@
 export const backgroundImage = [
-  "/img/login/login001.jpg",
-  "/img/login/login002.jpg",
-  "/img/login/login003.jpg",
+  "./img/login/login001.jpg",
+  "./img/login/login002.jpg",
+  "./img/login/login003.jpg",
 ];
